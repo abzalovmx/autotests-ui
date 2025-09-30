8e13394f8b055d4515b5b98b58fa9505eff4ce7d
Mirzaali Abzalov <mirzaali.abzalov@fido.uz>
2025-09-30 11:50:48 +0500

    Интеграция Pytest и Playwright

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,6 +1,7 @@
 def test_user_login():
     print('Hello!')
 
+
 class TestUserLogin:
 
     def test_1(self):
@@ -9,9 +10,10 @@ class TestUserLogin:
     def test_2(self):
         pass
 
+
 def test_assert_positive_case():
     assert (2 + 2) == 4
 
+
 def test_assert_negative_case():
     assert (2 + 2) == 5
-
