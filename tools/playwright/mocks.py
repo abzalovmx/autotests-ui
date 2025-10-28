4a1e1a8e5b40611c8ca7a174236cf78cc41674b6
Mirzaali Abzalov <mirzaali.abzalov@fido.uz>
2025-10-28 16:51:25 +0500

    Логирование взаимодействий со страницей

--- tools/playwright/mocks.py
+++ tools/playwright/mocks.py
@@ -2,4 +2,4 @@ from playwright.sync_api import Page
 
 
 def mock_static_resources(page: Page):
-    page.route("**/*.{ico,png,jpg,svg,webp,mp3,mp4,woff,woff2}", lambda route: route.abourt())
+    page.route("**/*.{ico,png,jpg,svg,webp,mp3,mp4,woff,woff2}", lambda route: route.abort())
