26be07f3f2ac021fa77cd525c1db77c07558f578
Mirzaali Abzalov <mirzaali.abzalov@fido.uz>
2025-10-29 13:25:30 +0500

    Запуск автотестов на CI

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class TestData(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env",
+        env_file="env",
         env_file_encoding="utf-8",
         env_nested_delimiter=".",
     )
