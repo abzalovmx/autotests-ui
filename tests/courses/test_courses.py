f47a8843da02585a58fe6f6ef86e0379713130f1
mirzaali.abzalov <mirzaali.abzalov@fido.uz>
2026-09-09 16:35:36 +0500

    7.7 Наращивание тестовой базы и рефакторинг. Практикуемся в написании автотестов с использованием Page Object, Page Component, Page Factory

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -62,3 +62,35 @@ class TestCourses:
             max_score='100',
             min_score='10'
         )
+
+    def test_edit_course(self, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
+        create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+        create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+        create_course_page.create_course_form.fill(
+            title="Python",
+            estimated_time="1 month",
+            description="Python Course",
+            max_score="250",
+            min_score="15"
+        )
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+
+        courses_list_page.course_view.check_visible(
+            index=0, title="Python", max_score="250", min_score="15", estimated_time="1 month"
+        )
+
+        courses_list_page.course_view.menu.menu_button.click()
+        courses_list_page.course_view.menu.edit_menu_button.click()
+
+        create_course_page.create_course_form.fill(
+            title="Playwright",
+            estimated_time="2 weeks",
+            description="Playwright",
+            max_score="100",
+            min_score="10"
+        )
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+
+        courses_list_page.course_view.check_visible(
+            index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
+        )
