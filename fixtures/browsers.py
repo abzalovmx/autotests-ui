04415b03c0f9191eab39ee0945159c8ab4c35ab0
mirzaali.abzalov <mirzaali.abzalov@fido.uz>
2026-09-18 16:48:53 +0500

    8.5 Добавление видео в Allure отчет

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,10 +1,10 @@
-import allure
 import pytest
 from playwright.sync_api import Page, Playwright
 
 from pages.authentication.registration_page import RegistrationPage
 from _pytest.fixtures import SubRequest
-from allure_commons.types import AttachmentType
+
+from tools.playwright.pages import initialize_playwright_page
 
 
 # Фикстура без использования pytest_learning-playwright встроенной фикстуры
@@ -19,20 +19,7 @@ from allure_commons.types import AttachmentType
 
 @pytest.fixture
 def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-
-    yield browser.new_page()
-
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(
-        f'./tracing/{request.node.name}.zip',
-        name='trace',
-        extension='zip'
-    )
+    yield from initialize_playwright_page(playwright, test_name=request.node.name)
 
 
 @pytest.fixture(scope="session")
@@ -58,18 +45,8 @@ def initialize_browser_state(playwright: Playwright):
 
 @pytest.fixture(scope="function")
 def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state='browser-state.json')
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-
-    yield context.new_page()
-
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-
-    browser.close()
-
-    allure.attach.file(
-        f'./tracing/{request.node.name}.zip',
-        name='trace',
-        extension='zip'
+    yield from initialize_playwright_page(
+        playwright,
+        test_name=request.node.name,
+        storage_state='browser-state.json'
     )
